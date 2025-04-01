2488015246c599f735f682862c41543e6cf21ad9
Matthew Corral <matthew_corral@dlsu.edu.ph>
2025-04-02 04:39:20 +0800

    Added package.json

--- apdev-phase2/server.js
+++ apdev-phase2/server.js
@@ -21,10 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
